06a67e9bd0fe5cee08f56ffe93d8c55f668b80e1
Nirmalselva <46560598+Nirmalselva@users.noreply.github.com>
2019-01-17 19:04:54 +0530

    Create largest of three.py

---
+++ largest of three.py
@@ -0,0 +1,9 @@
+a_ip = int(input())
+b_ip = int(input())
+c_ip = int(input())
+if a_ip>b_ip a_ipnd a_ip>c_ip :
+    print(a_ip);
+elif b_ip>a_ip a_ipnd b_ip>c_ip :
+    print(b_ip)
+else :
+    print(c_ip)
